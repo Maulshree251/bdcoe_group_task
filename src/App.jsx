dcd8a20291497a6c2d2d3b1a3b59e40e21503384
Maulshree251 <maulshreegupta2004@gmail.com>
2024-10-29 11:57:22 +0530

    added new file

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import LoginSignup from './components/loginSignup/loginSignup'
+ 
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      < LoginSignup />
+    </>
+  )
+}
+
+export default App
